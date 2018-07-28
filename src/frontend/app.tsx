60d0d8b64434efcc5a6a369a29eb038115d97efa
Torgeir Helgevold <torgeirh@hotmail.com>
2018-07-28 17:46:48 -0400

    more modules

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -9,6 +9,16 @@ import {FriendsList7Component} from './mod7/friends/friends';
 import {FriendsList8Component} from './mod8/friends/friends';
 import {FriendsList9Component} from './mod9/friends/friends';
 import {FriendsList10Component} from './mod10/friends/friends';
+import {FriendsList11Component} from './mod11/friends/friends';
+import {FriendsList12Component} from './mod12/friends/friends';
+import {FriendsList13Component} from './mod13/friends/friends';
+import {FriendsList14Component} from './mod14/friends/friends';
+import {FriendsList15Component} from './mod15/friends/friends';
+import {FriendsList16Component} from './mod16/friends/friends';
+import {FriendsList17Component} from './mod17/friends/friends';
+import {FriendsList18Component} from './mod18/friends/friends';
+import {FriendsList19Component} from './mod19/friends/friends';
+import {FriendsList20Component} from './mod20/friends/friends';
 
 
 export class AppComponent extends React.Component {
@@ -26,6 +36,16 @@ export class AppComponent extends React.Component {
         <FriendsList8Component></FriendsList8Component>
         <FriendsList9Component></FriendsList9Component>
         <FriendsList10Component></FriendsList10Component>
+        <FriendsList11Component></FriendsList11Component>
+        <FriendsList12Component></FriendsList12Component>
+        <FriendsList13Component></FriendsList13Component>
+        <FriendsList14Component></FriendsList14Component>
+        <FriendsList15Component></FriendsList15Component>
+        <FriendsList16Component></FriendsList16Component>
+        <FriendsList17Component></FriendsList17Component>
+        <FriendsList18Component></FriendsList18Component>
+        <FriendsList19Component></FriendsList19Component>
+        <FriendsList20Component></FriendsList20Component>
       </div>
     ); 
   }
