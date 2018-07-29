ead95db5a2b4daa7e506ccc10799f49e59ba3d42
Torgeir Helgevold <torgeirh@hotmail.com>
2018-07-29 15:05:53 -0400

    more modules

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 
-// import {ModComponent1} from './module1/mod';
-// import {ModComponent2} from './module2/mod';
-// import {ModComponent3} from './module3/mod';
-// import {ModComponent4} from './module4/mod';
-// import {ModComponent5} from './module5/mod';
-// import {ModComponent6} from './module6/mod';
-// import {ModComponent7} from './module7/mod';
-// import {ModComponent8} from './module8/mod';
-// import {ModComponent9} from './module9/mod';
-// import {ModComponent10} from './module10/mod';
-// import {ModComponent11} from './module11/mod';
-// import {ModComponent12} from './module12/mod';
-// import {ModComponent13} from './module13/mod';
-// import {ModComponent14} from './module14/mod';
-// import {ModComponent15} from './module15/mod';
-// import {ModComponent16} from './module16/mod';
-// import {ModComponent17} from './module17/mod';
+import {ModComponent1} from './module1/mod';
+import {ModComponent2} from './module2/mod';
+import {ModComponent3} from './module3/mod';
+import {ModComponent4} from './module4/mod';
+import {ModComponent5} from './module5/mod';
+import {ModComponent6} from './module6/mod';
+import {ModComponent7} from './module7/mod';
+import {ModComponent8} from './module8/mod';
+import {ModComponent9} from './module9/mod';
+import {ModComponent10} from './module10/mod';
+import {ModComponent11} from './module11/mod';
+import {ModComponent12} from './module12/mod';
+import {ModComponent13} from './module13/mod';
+import {ModComponent14} from './module14/mod';
+import {ModComponent15} from './module15/mod';
+import {ModComponent16} from './module16/mod';
+import {ModComponent17} from './module17/mod';
 import {ModComponent18} from './module18/mod';
 import {ModComponent19} from './module19/mod';
 import {ModComponent20} from './module20/mod';
@@ -26,7 +26,7 @@ export class AppComponent extends React.Component<IFriends, IFriends> {
   render(): any {
     return (
       <div>
-        {/* <ModComponent1></ModComponent1>
+        <ModComponent1></ModComponent1>
         <ModComponent2></ModComponent2>
         <ModComponent3></ModComponent3>
         <ModComponent4></ModComponent4>
@@ -43,7 +43,6 @@ export class AppComponent extends React.Component<IFriends, IFriends> {
         <ModComponent15></ModComponent15>
         <ModComponent16></ModComponent16>
         <ModComponent17></ModComponent17>
-         */}
         <ModComponent18></ModComponent18>
         <ModComponent19></ModComponent19>
         <ModComponent20></ModComponent20>
